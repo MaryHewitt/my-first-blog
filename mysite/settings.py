d0b187b8e6ce11d746dec7e00a7c7e503203d4a6
MaryHewitt <mary.hewitt@postgrad.curtin.edu.au>
2021-11-13 12:07:39 +0800

    turned debug mode off

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^$ltds2tsd=(txfo63ed^&!90p=utzei5i^8aaynw)ax+fgeb2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
