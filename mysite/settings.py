e3910bfd2d88977411bd7c3a8b1f8a3fab5476db
MaryHewitt <mary.hewitt@postgrad.curtin.edu.au>
2021-11-13 14:00:26 +0800

    pretty styling

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^$ltds2tsd=(txfo63ed^&!90p=utzei5i^8aaynw)ax+fgeb2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
@@ -119,4 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+#print(STATIC_ROOT)
